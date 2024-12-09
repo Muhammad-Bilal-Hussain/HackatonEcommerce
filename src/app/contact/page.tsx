173ae9af52c3586b08fc193ae2c420e22e829c23
Muhammad Bilal Hussain <bh1977955@gmail.com>
2024-12-10 01:26:12 +0500

    update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -11,114 +11,159 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocation } from '@fortawesome/free-solid-svg-icons/faLocation';
 import { faLocationArrow } from '@fortawesome/free-solid-svg-icons/faLocationArrow';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons/faLocationDot';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons/faPhone';
 
 function page() {
   return (
     <div>
         {/* navbar  */}
-        <section id='Shop'>
-        <div className='max-w-full bg-[#ffffff]'>
-        <div className='max-w-7xl mx-auto py-10 px-10'>
-  <nav className="flex justify-between items-center px-8 py-4 ">
-    {/* Left Menu */}
-    {/* Navigation Links */}
-    <ul className="flex space-x-24 text-gray-700">
-      <li>
-      <Link href={"/"} className="hover:text-black text-lg font-bold">
-      Home</Link>
-      </li>
-      <li>
-      <Link href={"/shop"} className="hover:text-black text-lg font-bold">
-      Shop</Link>
-      </li>
-      <li>
-        <a href="#" className="hover:text-black text-lg font-bold">
-          About
-        </a>
-      </li>
-      <li>
-        <a href="#" className="hover:text-black text-lg font-bold">
-          Contact
-        </a>
-      </li>
-    </ul>
-    {/* Icons */}
-    <div className="flex space-x-10 text-gray-700">
-      <span className="material-icons"><Image src={manicon} alt='icon' className='w-[50px] cursor-pointer'></Image></span>
-      <span className="material-icons "><Image src={manicon1} alt='icon' className='w-[50px] cursor-pointer'></Image></span>
-      <span className="material-icons"><Image src={manicon2} alt='icon' className='w-[50px] cursor-pointer'></Image></span>
-      <span className="material-icons"><Image src={manicon3} alt='icon' className='w-[50px] cursor-pointer'></Image></span>
-    </div>
-  </nav>
-    </div>
-    </div>
-        </section>
-        {/* shop  hero div*/}
-        <div className='max-w-full relative'>
-        <Image src={shopbg} alt='bg pic instagram' className='w-full h-[700px] object-cover opacity-25'/>
-        <div className='max-w-7xl mx-auto py-10 px-10'>
-          <div className='flex justify-center items-center absolute inset-0'>
-          <div className='text-center'>
-            <div className='flex justify-center items-center'>
-            <Image src={shopicon} alt='shop icon' className='w-[120px]'></Image>
-            </div>
-            <h1 className='text-6xl font-medium'>Contact</h1>
-            <p className='text-3xl py-5'><span className='font-semibold text-3xl'>Home</span> &gt; Contact </p>
-          </div>
-          </div>
+        <section>
+  <div className="max-w-full bg-[#ffffff]">
+    <div className="max-w-7xl mx-auto py-6 px-6 lg:px-10">
+      <nav className="flex flex-wrap justify-between items-center px-4 py-3">
+        {/* Navigation Links */}
+        <ul className="flex flex-wrap justify-center space-x-6 lg:space-x-24 text-gray-700 text-sm lg:text-lg">
+          <li>
+            <Link href={"/"} className="hover:text-black font-bold">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href={"/shop"} className="hover:text-black font-bold">
+              Shop
+            </Link>
+          </li>
+          <li>
+            <a href="#" className="hover:text-black font-bold">
+              About
+            </a>
+          </li>
+          <li>
+            <Link href={"/contact"} className="hover:text-black font-bold">
+              Contact
+            </Link>
+          </li>
+        </ul>
+        {/* Icons */}
+        <div className="flex flex-wrap justify-center space-x-4 lg:space-x-10 text-gray-700 mt-4 lg:mt-0">
+          <Image src={manicon} alt="icon" className="w-8 lg:w-[50px] cursor-pointer" />
+          <Image src={manicon1} alt="icon" className="w-8 lg:w-[50px] cursor-pointer" />
+          <Image src={manicon2} alt="icon" className="w-8 lg:w-[50px] cursor-pointer" />
+          <Image src={manicon3} alt="icon" className="w-8 lg:w-[50px] cursor-pointer" />
         </div>
-      </div>
-      {/* contact  */}
-      <div className='max-w-full'>
-        <div className='max-w-7xl mx-auto py-10 px-10'>
-            <div className='flex justify-center items-center p-10 max-w-6xl'>
-                <div className='text-center'>
-                <h1 className='text-6xl font-bold'>Get In Touch With Us</h1>
-                <p className='text-2xl text-gray-400'>For More Information About Our Product & Services. Please Feel Free To Drop Us An Email. Our Staff Always Be There To Help You Out. Do Not Hesitate!</p>
-                </div>
-            </div>
-            <div className='flex justify-between items-center'>
-                <div className='space-y-16'>
-                    {/* address  */}
-                <div className='w-[420px] space-y-5'>
-                    <p className='text-5xl font-semibold'><FontAwesomeIcon icon={faLocationDot} className='mr-5'/>Address</p>
-                    <p className='text-3xl ml-16'>236 5th SE Avenue, New York NY10000, United States</p>
-                </div>
-                {/* phone  */}
-                <div className='w-[420px] space-y-5'>
-                    <p className='text-5xl font-semibold'><FontAwesomeIcon icon={faLocationDot} className='mr-5'/>Phone</p>
-                    <p className='text-3xl ml-16'>Mobile: +(84) 546-6789</p>
-                    <p className='text-3xl ml-16'>Hotline: +(84) 546-6789</p>
-                </div>
-                {/* work time  */}
-                <div className='w-[420px] space-y-5'>
-                    <p className='text-5xl font-semibold'><FontAwesomeIcon icon={faLocationDot} className='mr-5'/>Working Time</p>
-                    <p className='text-3xl ml-16'>Monday-Friday: 9:00 - 22:00</p>
-                    <p className='text-3xl ml-16'>Saturday-Sunday: 9:00 - 21:00</p>
-                </div>
-                </div>
-                <div className='space-y-16'>
-                    <div className=''>
-                    <label htmlFor="name" className='block text-2xl font-semibold'>Your Name</label>
-                    <input type="text" placeholder='abc' className="w-[400px] h-[70px] mt-2 p-3 rounded-lg border-2 border-gray-300  text-base py-1 leading-8 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-                    </div>
-                    <div className=''>
-                    <label htmlFor="name" className='block text-2xl font-semibold'>Email Address</label>
-                    <input type="text" placeholder='abc@def.com' className="w-[400px] h-[70px] mt-2 p-3 rounded-lg border-2 border-gray-300  text-base py-1 leading-8 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-                    </div>
-                    <div className=''>
-                    <label htmlFor="name" className='block text-2xl font-semibold'>Subject</label>
-                    <input type="text" placeholder='this an optional' className="w-[400px] h-[70px] mt-2 p-3 rounded-lg border-2 border-gray-300  text-base py-1 leading-8 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-                    </div>
-                    <div className=''>
-                    <label htmlFor="name" className='block text-2xl font-semibold'>Message</label>
-                    <textarea  placeholder='abc' className="w-[400px] h-[70px] mt-2 p-3 rounded-lg border-2 border-gray-300  text-base py-1 leading-8 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-                    </div>
-                    <button className='bg-white text-black border-2 border-gray-400 rounded-xl text-center w-[200px] h-[45px]'>Submit</button>
-                </div>
-            </div>
+      </nav>
+    </div>
+  </div>
+</section>
+
+{/* Shop Hero Section */}
+<div className="max-w-full relative">
+  <Image
+    src={shopbg}
+    alt="bg pic instagram"
+    className="w-full h-[400px] lg:h-[700px] object-cover opacity-25"
+  />
+  <div className="max-w-7xl mx-auto py-6 px-6 lg:px-10">
+    <div className="flex justify-center items-center absolute inset-0">
+      <div className="text-center">
+        <div className="flex justify-center items-center">
+          <Image src={shopicon} alt="shop icon" className="w-16 lg:w-[100px]" />
         </div>
+        <h1 className="text-3xl lg:text-6xl font-medium">Contact</h1>
+        <p className="text-sm lg:text-xl font-semibold py-3 lg:py-5">
+          <span className="font-bold">Home</span> &gt; Contact
+        </p>
       </div>
+    </div>
+  </div>
+</div>
+{/* Contact */}
+<div className='max-w-full'>
+  <div className='max-w-7xl mx-auto py-10 px-4 sm:px-6 lg:px-10'>
+    <div className='text-center p-10'>
+      <h1 className='text-4xl sm:text-5xl md:text-6xl font-bold'>Get In Touch With Us</h1>
+      <p className='text-xl sm:text-2xl text-gray-400 mt-4'>
+        For More Information About Our Product & Services. Please Feel Free To Drop Us An Email. Our Staff Will Always Be There To Help You Out. Do Not Hesitate!
+      </p>
+    </div>
+
+    {/* Flexbox for large screens */}
+    <div className='lg:flex lg:justify-between lg:space-x-10 lg:space-y-0 space-y-10'>
+      {/* Address */}
+      <div className='w-full sm:w-[420px] lg:w-[420px] space-y-5'>
+        <p className='text-3xl sm:text-4xl font-semibold'>
+          <FontAwesomeIcon icon={faLocationDot} className='mr-3' />
+          Address
+        </p>
+        <p className='text-xl sm:text-2xl'>236 5th SE Avenue, New York NY10000, United States</p>
+      </div>
+
+      {/* Phone */}
+      <div className='w-full sm:w-[420px] lg:w-[420px] space-y-5'>
+        <p className='text-3xl sm:text-4xl font-semibold'>
+          <FontAwesomeIcon icon={faPhone} className='mr-3' />
+          Phone
+        </p>
+        <p className='text-xl sm:text-2xl'>Mobile: +(84) 546-6789</p>
+        <p className='text-xl sm:text-2xl'>Hotline: +(84) 546-6789</p>
+      </div>
+
+      {/* Working Time */}
+      <div className='w-full sm:w-[420px] lg:w-[420px] space-y-5'>
+        <p className='text-3xl sm:text-4xl font-semibold'>
+          <FontAwesomeIcon icon={faClock} className='mr-3' />
+          Working Time
+        </p>
+        <p className='text-xl sm:text-2xl'>Monday-Friday: 9:00 - 22:00</p>
+        <p className='text-xl sm:text-2xl'>Saturday-Sunday: 9:00 - 21:00</p>
+      </div>
+    </div>
+
+    {/* Form Section */}
+    <div className='space-y-10 mt-10'>
+      <div className=''>
+        <label htmlFor="name" className='block text-2xl font-semibold'>Your Name</label>
+        <input
+          type="text"
+          placeholder='abc'
+          className="w-full sm:w-[400px] lg:w-[400px] h-[70px] mt-2 p-3 rounded-lg border-2 border-gray-300 text-base py-1 leading-8 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+
+      <div className=''>
+        <label htmlFor="email" className='block text-2xl font-semibold'>Email Address</label>
+        <input
+          type="email"
+          placeholder='abc@def.com'
+          className="w-full sm:w-[400px] lg:w-[400px] h-[70px] mt-2 p-3 rounded-lg border-2 border-gray-300 text-base py-1 leading-8 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+
+      <div className=''>
+        <label htmlFor="subject" className='block text-2xl font-semibold'>Subject</label>
+        <input
+          type="text"
+          placeholder='This is optional'
+          className="w-full sm:w-[400px] lg:w-[400px] h-[70px] mt-2 p-3 rounded-lg border-2 border-gray-300 text-base py-1 leading-8 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+
+      <div className=''>
+        <label htmlFor="message" className='block text-2xl font-semibold'>Message</label>
+        <textarea
+          placeholder='Your message here'
+          className="w-full sm:w-[400px] lg:w-[400px] h-[150px] mt-2 p-3 rounded-lg border-2 border-gray-300 text-base py-1 leading-8 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+
+      <button className='bg-blue-600 text-white border-2 border-gray-400 rounded-xl text-center w-full sm:w-[200px] lg:w-[200px] h-[45px] mt-6'>
+        Submit
+      </button>
+    </div>
+  </div>
+</div>
+
             {/* free delivery line  */}
             <div className='max-w-full bg-[#fbf3f3] py-10 mt-10'> 
         <div className='max-w-[120rem] mx-auto py-10 px-10'>
